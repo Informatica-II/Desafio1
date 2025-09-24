9ebb339c976bfa1a5ebf05bea4c5cfb502f3db5a
Informatica-II <oscar.sepulveda2@udea.edu.co>
2025-09-24 12:21:02 -0500

    Leer archivo
    Leer archivo y verificar tamaño

--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    ifstream archivo;
+    string linea;
+    int tam = 0;
 
-    cout << "Probando repositorio" << endl;
+    archivo.open("Encriptado1.txt");  //Abrir el archivo para lectura
+    if (!archivo.is_open()) {
+        cout << "Falla al abrir el archivo." << endl;
+        return 1;  // Exit the program with an error code
+    }
+    archivo.seekg(0, archivo.end);
+    tam = archivo.tellg();
+    archivo.seekg(0, archivo.beg);
+    cout << "Este es el tamanio: " << tam << endl;
+
+    string *archi = new string[tam];
+
+    while (getline(archivo, linea)) { // Lee línea por línea
+        *archi += linea;
+        //cout << linea << endl; // Muestra la línea en la consola
+    }
+    archivo.close();
+
+    cout << *archi << endl;
+
+    delete[] archi;
+
+    return 0;
 }
